35b395a5b3c6ac063f7b7430d5e1faeb2f46407d
Charlly PIERRE <charlly.pierre@alumni.univ-avignon.fr>
2021-04-07 19:20:39 +0200

    Initial commit

---
+++ app/src/main/java/fr/uavignon/ceri/tp3/DetailViewModel.java
@@ -0,0 +1,32 @@
+package fr.uavignon.ceri.tp3;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import fr.uavignon.ceri.tp3.data.City;
+import fr.uavignon.ceri.tp3.data.WeatherRepository;
+
+public class DetailViewModel extends AndroidViewModel {
+    public static final String TAG = DetailViewModel.class.getSimpleName();
+
+    private WeatherRepository repository;
+    private MutableLiveData<City> city;
+
+    public DetailViewModel (Application application) {
+        super(application);
+        repository = WeatherRepository.get(application);
+        city = new MutableLiveData<>();
+    }
+
+    public void setCity(long id) {
+        repository.getCity(id);
+        city = repository.getSelectedCity();
+    }
+    LiveData<City> getCity() {
+        return city;
+    }
+}
+
